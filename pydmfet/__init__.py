ebf9c507c5070115d22d6f554d352092a7446e08
fishjojo <xingz@tigercpu.princeton.edu>
2019-04-26 19:25:28 -0400

    update

--- pydmfet/__init__.py
+++ pydmfet/__init__.py
@@ -10,7 +10,7 @@ from distutils.version import LooseVersion
 import scipy
 if LooseVersion(scipy.__version__) < LooseVersion('1.0.0'):
     raise SystemError("You're using an old version of Scipy (%s). "
-                      "It is recommended to upgrad scipy to 1.0.0 or newer to support pydmfet." %
+                      "It is recommended to upgrade scipy to 1.0.0 or newer to support pydmfet." %
                       scipy.__version__)
 
 __path__.append(os.path.join(os.path.dirname(__file__), 'tools'))
